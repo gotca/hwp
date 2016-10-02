9f5fce5b54eb2f6558c195bf18c44feb700af06b
Aron Duby <aron.duby@gmail.com>
2016-10-02 16:44:43 -0400

    Update the photo importer with new structure

--- database/seeds/import/Photos.php
+++ database/seeds/import/Photos.php
@@ -17,7 +17,6 @@ class Photos extends ImportBase
         $this->rename($data, 'photo_id', 'file');
         $this->rename($data, 'created', 'created_at');
         $data->season_id = $data->file[1];
-        $data->shutterfly_node_id = substr($data->file, strpos($data->file, "p") + 1);
 
         unset($data->viewed);
 
