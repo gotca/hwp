97d00d17ccae3ea73f2d730ea071f0e3e0a8717b
Aron Duby <aron.duby@gmail.com>
2016-10-09 00:45:31 -0400

    cast the rank properties because production was treating them all as strings in json conversion

--- app/Models/Rank.php
+++ app/Models/Rank.php
@@ -10,6 +10,17 @@ class Rank extends Model
     use BelongsToTenant;
     
     protected $fillable = ['rank', 'team', 'tied', 'self'];
+    
+    protected $casts = [
+        'site_id' => 'integer',
+        'season_id' => 'integer',
+        'ranking_id' => 'integer',
+        'rank' => 'integer',
+        'self' => 'boolean',
+        'tied' => 'boolean',
+    ];
 
     public $timestamps = false;
+    
+    
 }
