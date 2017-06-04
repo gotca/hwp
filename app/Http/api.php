b1b7e4a1ac00c48e98cce883be56daf85c48595d
Aron Duby <aron.duby@gmail.com>
2017-06-04 17:32:01 -0400

    tweak the data returned for the VR call

--- app/Http/api.php
+++ app/Http/api.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('vr-data/{player}', function(\App\Models\Player $player) {
     $data = DB::table('stats')
-        ->leftJoin('games', 'stats.game_id', '=', 'games.id')
-        ->leftJoin('seasons', 'stats.season_id', '=', 'seasons.id')
+        ->join('games', 'stats.game_id', '=', 'games.id')
+        ->join('seasons', 'stats.season_id', '=', 'seasons.id')
         ->select('stats.*', 'games.*', 'seasons.title AS season_title', 'seasons.short_title AS season_short_title')
         ->where('stats.player_id', $player->id)
         ->get();
