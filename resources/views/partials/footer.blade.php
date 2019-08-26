edaccbaa51d7615772911789ef2b442fe4657f1e
Aron Duby <aron.duby@gmail.com>
2019-08-25 21:18:12 -0400

    update twitter embed code to new stuff

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,16 +12,15 @@
             <div class="row">
                 <div class="col-xs-12 col-md-4">
                     <section class="twitter-embed">
-                        <a
-                            class="twitter-timeline"
-                            href="https://twitter.com/HHSWaterPolo"
-                            data-widget-id="384422471509618689"
-                            data-link-color="#6ba3d0"
-                            data-chrome="noheader nofooter noborders transparent"
-                            data-tweet-limit="1"
-                            data-theme="dark"
-                        >Tweets by @HHSWaterPolo</a>
-                        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                        <a class="twitter-timeline"
+                           href="https://twitter.com/HHSWaterPolo?ref_src=twsrc%5Etfw"
+                           data-dnt="true"
+                           data-link-color="#6ba3d0"
+                           data-chrome="noheader nofooter noborders transparent"
+                           data-tweet-limit="1"
+                           data-theme="dark"
+                       >Tweets by HHSWaterPolo</a>
+                       <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     </section>
                 </div>
                 <div class="col-xs-12 col-md-8">
